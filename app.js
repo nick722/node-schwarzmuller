1090c2d23e178fc182ae945dd1719ef9462e4574
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2020-02-11 22:22:12 +0300

    066 Adding a 404 Error Page

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRouter);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1>404. Page not found</h1>");
+});
+
 app.listen(3030);
